ff490cb94f5e12cf3f59e96356df02aa079c0525
Riddhi-Chatterjee <riddhich@rediffmail.com>
2022-11-04 12:45:40 +0530

    trying out jenkins

--- DUMMY.py
+++ DUMMY.py
@@ -2,7 +2,6 @@ print("Running infinite loop...")
 i=0
 while(True):
     i = (i + 1) % 100000000
-    if i%1000000 == 0:
-        with open("DUMMY.txt", "a") as d:
-            d.write("Current value of i: "+str(i)+"\n")
+    with open("DUMMY.txt", "a") as d:
+        d.write("Current value of i: "+str(i)+"\n")
     break
